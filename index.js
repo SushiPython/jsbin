063eb299f9404f31b99529044c3bc7d9002ad2dc
Sushi <57518856+SushiPython@users.noreply.github.com>
2021-02-28 22:37:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks')
 const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient;
+const db = require('./db')
 
 const app = express();
 
@@ -13,49 +13,18 @@ nunjucks.configure('views', {
 
 app.use(bodyParser.json());
 
-function makeid(length) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
-}
- 
-let client
-let coll
-
-async function connect() {
-  client = await MongoClient.connect(process.env.url, { useNewUrlParser: true, useUnifiedTopology: true })
-  coll = client.db('main').collection('main')
-}
-
-connect()
-
-app.get('/', (req, res) => {
-  res.render('index.html')
-});
-
-app.get('/bins/:bins', (req, res) => {
-  let binId = req.params.bins
-  console.log(binId)
-  coll.findOne({"id": binId}, function(err, result) {
-    if (err) throw err;
-    console.log(result['text'])
-    res.render('bins.njk', {text: result['text']})
-  })
+app.get('/bins/:bins', async(req, res) => {
+  let doc = await db.findBin(req.params.bins)
+  res.render('index.html', {text: doc['text']})
 })
 
+app.get('/', async(req, res) => {
+  res.render('index.html', {text: ''})
+})
 
-app.post('/upload', (req, res) => {
-  let body = req.body
-  let id = makeid(8)
-  coll.insertOne({
-    "text": body['data'],
-    'id': id
-  })
-  res.send('{"id": "'+id+'"}')
+app.post('/upload', async(req, res) => {
+  let hash = await db.addBin(req.body['data'])
+  res.send('{"id": "'+hash+'"}')
 });
 
 app.listen(3000, () => {
